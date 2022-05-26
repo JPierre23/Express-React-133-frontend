5886f1680b356e680fdff2d0fc56144b730cb1c4
Jonathan Pierre <JonWPierre@gmail.com>
2022-05-26 13:31:31 -0400

    updated package.json

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -16,8 +16,9 @@ function Projects(props) {
 
   // make an initial call for the data inside a useEffect, so it only happens once on component load
   useEffect(() => 
-  async function (){await getProjectsData()}, 
+   { getProjectData()}, 
   []);
+
   // define a function that will return the JSX needed once we get the data
   const loaded = () => {
     return projects.map((project,index) => (
